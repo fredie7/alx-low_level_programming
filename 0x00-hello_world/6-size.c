655c103df6df472c2225cae09c3ca443c3c8cf78
fredie7 <lexfrederick3@gmail.com>
2022-06-02 12:45:58 -0700

    Print the size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,34 +1,23 @@
 #include <stdio.h>
-
-int main(void){
-  
-  char char_capacity;
-  
-  int int_capacity;
-  
-  long int longInt_capacity;
-  
-  long long int longLongInt_capacity;
-  
-  float float_capacity;
-  
-
-  
-  printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char_capacity));
-  
-  printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int_capacity));
-  
-  printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longInt_capacity));
-  
-  printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(longLongInt_capacity));
-  
-  printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float_capacity));
-  
-
-  
-
-  
-  return 0;
-  
+/**
+ * main - first point
+ * Return: always 0
+ */
+int main(void)
+{
+char char_capacity;
+int int_capacity;
+long int longInt_capacity;
+long long int longLongInt_capacity;
+float float_capacity;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char_capacity));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int_capacity));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)
+sizeof(longInt_capacity));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)
+sizeof(longLongInt_capacity));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)
+sizeof(float_capacity));
+return (0);
 }
 
