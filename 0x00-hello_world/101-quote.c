3aa68b679c2eed38a54ef70a2a1d3a37278ab700
fredie7 <lexfrederick3@gmail.com>
2022-06-02 13:53:42 -0700

    Print to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,10 @@
  * main - first point
  * Return: always 0
 */
-int main()
+int main(void)
 {
-fprintf(stderr, "and that piece of art is
-useful\" - Dora Korpar, 2015-10-19\n");
+char message[] = "and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19";
+printf("%s\n", message);
 return (1);
 }
