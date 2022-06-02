7304ee3a48639988525ff1f40ef5a632e73f356a
fredie7 <lexfrederick3@gmail.com>
2022-06-02 12:17:24 -0700

    prnt data types

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-
-int main(void){
-  
-      puts("\"Programming is like building a multilingual puzzle");
-  
-      return 0;
-  
+/**
+ * main - first point
+ * Return: always 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
 
