f28782cc32d7181daf24f48b89a43d2b2dcee4c4
fredie7 <lexfrederick3@gmail.com>
2022-06-03 08:03:58 -0700

    Print lower and upper cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - first point
+ * Return: always 0
+ */
+int main(void){
+char alphabet = 'a';
+while(alphabet < 'z')
+{
+putchar(alphabet);
+alphabet++;
+}
+alphabet = 'A';
+while(alphabet < 'Z')
+{
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+return (0);
+}
