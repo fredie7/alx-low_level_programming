80459361eaf2f1a673ef9f3a9d0bec7626da8082
fredie7 <lexfrederick3@gmail.com>
2022-06-03 04:38:40 -0700

    Find logical relationship

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,6 @@ int lastDigit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 lastDigit = n % 10;
-int a = 5;
 if(lastDigit > 5){
 printf("Last digit of %d is %d and is greater than 5\n",n,lastDigit);
 }
