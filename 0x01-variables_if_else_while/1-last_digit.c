ce3ea58f06a3e3f303c569427bf8e7ee00154dfd
fredie7 <lexfrederick3@gmail.com>
2022-06-03 04:31:07 -0700

    Find logical relationship

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - first point
+ * Return: always 0
+ */
+int main(void)
+{	
+int n;
+int lastDigit;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastDigit = n % 10;
+int a = 5;
+if(lastDigit > 5){
+printf("Last digit of %d is %d and is greater than 5\n",n,lastDigit);
+}
+if(lastDigit == 0){
+printf("Last digit of %d is %d and is 0\n",n,lastDigit);
+}
+if(lastDigit < 6 && lastDigit != 0){
+printf("Last digit of %d is %d and is less than 6 and not 0\n",n,lastDigit);
+}
+return (0);
+}
