4007fe12c3af3c31476a881bec3a82e1af94e0c1
fredie7 <lexfrederick3@gmail.com>
2022-06-03 05:48:58 -0700

    Print lower cases

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - first point
+ * Return: always 0
+ */
+int main(void)
+{
+char alphabet;
+for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+putchar('\n');
+}
+return (0);
+}
