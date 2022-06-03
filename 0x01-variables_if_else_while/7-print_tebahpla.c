98c337ec5c0b27b22ae1a0e1c2da9b5657a2707b
fredie7 <lexfrederick3@gmail.com>
2022-06-03 14:10:03 -0700

    Print in revrse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - first point
+ * Return: always 0
+ */
+int main(void)
+{
+char alphabet = 'z';
+while (alphabet >= 'a')
+{
+putchar(alphabet);
+alphabet--;
+}
+putchar('\n');
+return 0;
+}
