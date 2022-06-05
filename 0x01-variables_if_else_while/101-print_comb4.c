fd2521d98490b5db33021ad4d360b782c6d3e2df
fredie7 <lexfrederick3@gmail.com>
2022-06-05 10:03:17 -0700

    Print 3 digits combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,22 +6,27 @@ int main(void)
 {
 int a;
 int b;
-for (a = 0; a < 10; a++)
+int c;
+for (a = 0; a <= 9; a++)
 {
-for (b = a + 1; b < 10; b++)
+for (b = 0; b <= 9; b++)
+{
+for (c = 0; c <= 9; c++)
+{
+if (a != b && b != c && a != c && a < b && c > b)
 {
 putchar(a + '0');
 putchar(b + '0');
-if((a == 8) && (b == 9))
-
-;
-else
+putchar(c + '0');
+if (!(a == 7 && b == 8 && c == 9))
 {
 putchar(',');
 putchar(' ');
 }
-}	
-}	
+}
+}
+}
+}
 putchar('\n');
 return (0);
 }
