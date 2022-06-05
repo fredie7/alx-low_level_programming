a398a0ee9b5f48f7d831bb77d4ad5900ba8360ce
fredie7 <lexfrederick3@gmail.com>
2022-06-05 09:51:38 -0700

    Print combnations of 2 digit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+/**
+ * main - first point
+ * Return: always 0
+ */
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+int main(void)
+{
+int a;
+int b;
+for (a = 0; a < 10; a++)
+{
+for (b = a + 1; b < 10; b++)
+{
+putchar(a + '0');
+putchar(b + '0');
+if((a == 8) && (b == 9))
+
+;	
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
