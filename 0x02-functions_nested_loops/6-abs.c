55e389edf8c094ea54e725551ef3effa83c2a636
fredie7 <lexfrederick3@gmail.com>
2022-06-07 16:27:01 -0700

    Compute absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+/**
+ * _isalpha - checks for upper case letters
+ * @num: the character to check for upper case letters
+ * Description: checks for upper case letters.
+ * Return: 1 if alphabet is upper case. 0 otherwise
+ **/
+
+int _abs(int num)
+{
+return (abs(num));
+}
+
