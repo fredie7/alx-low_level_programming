f3f4cc21aa103b3cd526b611a9f4ecc8f6dcf4ed
fredie7 <lexfrederick3@gmail.com>
2022-06-07 06:04:02 -0700

    Print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - first point
+ * Return: always 0
+ */
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
