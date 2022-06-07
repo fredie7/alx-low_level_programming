8c51457d89901b2602b5e1f3a3f1d3a37b819091
fredie7 <lexfrederick3@gmail.com>
2022-06-07 05:28:51 -0700

    Print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,12 +6,14 @@
  * main - first point
  * Return: always 0
  */
-int main(void){
+int main(void)
+{
 char num[] = "_putchar";
 int i;
-for(i=0;i<8;i++){
+for (i = 0; i < 8; i++)
+{
 _putchar(num[i]);
 }
 putchar('\n');
-return(0);
+return (0);
 }
