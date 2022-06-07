7f10673845d2daae37ed08dcba29aa0db70a425e
fredie7 <lexfrederick3@gmail.com>
2022-06-07 15:57:59 -0700

    Check for upper case letters

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <ctype.h>
 /**
- * _isupper - checks for upper case letters
+ * _isalpha - checks for upper case letters
  * @c: the character to check for upper case letters
  * Description: checks for upper case letters.
  * Return: 1 if alphabet is upper case. 0 otherwise
  **/
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
