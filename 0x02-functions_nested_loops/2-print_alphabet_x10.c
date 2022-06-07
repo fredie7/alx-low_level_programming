54a962c22ecd8c24d8356d2d208c9c9ed2e4d793
fredie7 <lexfrederick3@gmail.com>
2022-06-07 08:19:18 -0700

    print letters multiple tmes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,19 @@
 #include "main.h"
 /**
- * main - first point
- * Return: always 0
+ * print_alphabet - Print letters multiple times
+ * Description: Print letters multiple times
+ * Return: void
  */
+void print_alphabet_x10(void)
+{
+char alphabet;
+for(int i = 0; i <= 10; i++)
+{
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+}
+_putchar('\n');
+return (0);
+}
