0a5068d202f21ba845f0d80879eb15e46fcfcf25
fredie7 <lexfrederick3@gmail.com>
2022-06-07 19:01:50 -0700

    Print numbers from  to 100

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * void print_to_98 - checks for lower case character
+ * @n: the character to check for
+ * Description: checks for lowercase character.
+ * Return: 1 if c is lower case. 0 otherwise
+ **/
+void print_to_98(int n)
+{
+int i;
+i = 0;
+n = 99;
+while(i<n){
+_putchar(i);
+_putchar(32);
+i++;
+}
+}
