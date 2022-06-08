497684d357e08308a555ed555d646fa4aee3ac73
fredie7 <lexfrederick3@gmail.com>
2022-06-08 05:22:21 -0700

    Prints al natural numbers before 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <studio.h>
 /**
  * void print_to_98 - Prints all natural numbers before 98
  * @c: the character to check for
@@ -9,11 +10,10 @@ void print_to_98(int c)
 {
 int i;
 i = 0;
-c = 98;
 while (i<c){
-_putchar((i + '0'));
-_putchar(44);
-_putchar(32);
+prntf("%d",i);
+putchar(44);
+putchar(32);
 i++;
 }
 }
