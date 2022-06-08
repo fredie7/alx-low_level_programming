7dcfff7a9e1f6c9edc52474d9ebd465316076aeb
fredie7 <lexfrederick3@gmail.com>
2022-06-08 06:52:41 -0700

    Print times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,16 +1,37 @@
 #include "main.h"
+
 /**
- * _islower - checks for lower case character
- * @c: the character to check for
- * Description: checks for lowercase character.
- * Return: 1 if c is lower case. 0 otherwise
- **/
+ * times_table - prints the 9 times table, starting with 0
+ * Return: no return
+ */
 void times_table(void)
 {
-int i;
-int n;
-n = 9;
-for (i = 1; i <= 10; ++i) {
-_putchar( n * i);
+int a;
+int b;
+int c;
+for (a = 0; a < 10; a++)
+{
+for (b = 0; b < 10; b++)
+{
+z = b * a;
+if (b == 0)
+{
+_putchar(z + '0');
+}
+if (z < 10 && b != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(z + '0');
+} else if (z >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((z / 10) + '0');
+_putchar((z % 10) + '0');
+}
+}
+_putchar('\n');
 }
 }
