7063780ed7c024b35e38910e9b65ce30796e7bc1
fredie7 <lexfrederick3@gmail.com>
2022-06-07 18:30:17 -0700

    Print multipliction table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - checks for lower case character
+ * @c: the character to check for
+ * Description: checks for lowercase character.
+ * Return: 1 if c is lower case. 0 otherwise
+ **/
+void times_table(void)
+{
+int i;
+int n;
+n = 9;
+for (i = 1; i <= 10; ++i) {
+_putchar( n * i);
+}
+}
