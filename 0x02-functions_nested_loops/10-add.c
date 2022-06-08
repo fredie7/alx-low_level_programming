8137ffa819f48d0e2fd518cbb1cb8835a5f4b7ed
fredie7 <lexfrederick3@gmail.com>
2022-06-07 18:45:12 -0700

    Add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _add - checks for lower case character
+ * @a: the character to check for
+ * @b: the character to check for
+ * Description: checks for lowercase character.
+ * Return: 1 if c is lower case. 0 otherwise
+ **/
+int add(int a, int b)
+{
+return (a + b);
+}
