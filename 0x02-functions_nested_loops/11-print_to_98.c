6bc7a17d5286b16f63c4979d06d280cfa8f986aa
fredie7 <lexfrederick3@gmail.com>
2022-06-08 04:26:41 -0700

    Prints al natural numbers before 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
- * void print_to_98 - checks for lower case character
- * @n: the character to check for
- * Description: checks for lowercase character.
- * Return: 1 if c is lower case. 0 otherwise
+ * void print_to_98 - Prints all natural numbers before 98
+ * @c: the character to check for
+ * Description: Prints all natural numbers before 98
+ * Return: all natural numbers before 98
  **/
-void print_to_98(int n)
+void print_to_98(int c)
 {
 int i;
 i = 0;
-n = 99;
+int n = 99;
 while(i<n){
-_putchar(i);
+_putchar((i % 100) + '0');
+_putchar(44);
 _putchar(32);
 i++;
 }
