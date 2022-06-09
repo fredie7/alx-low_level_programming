e847c12e8a574f7e16225db6d901b31aec50d103
fredie7 <lexfrederick3@gmail.com>
2022-06-09 16:39:23 -0700

    Print an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @num: resulting integer
+ */
+void print_number(int num)
+{
+unsigned int n;
+if (num < 0)
+{
+n = -num;
+_putchar('-');
+}
+else
+{
+n = num;
+}
+if (n / 10)
+{
+print_number(n / 10);
+}
+_putchar((n % 10) + '0');
+}
