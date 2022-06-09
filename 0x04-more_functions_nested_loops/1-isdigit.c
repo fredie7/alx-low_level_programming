2fb99a04b742c06606dc52303b327130de4c40b3
fredie7 <lexfrederick3@gmail.com>
2022-06-09 06:53:20 -0700

    Check for digits

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <ctype.h>
 /**
- * _isdigit - checks for upper case letters
- * @c: the character to check for digits from 0 through 9
- * Description: checks for upper case letters.
- * Return: 1 if c is digit. 0 otherwise
+ * _isdigit - checks for digits
+ * @c: the character to check for
+ * Description: checks for digits.
+ * Return: 1 if c is a digit. 0 otherwise
  **/
 int _isdigit(int c)
 {
-return (c >= 0 && c <= 9);
+return (c >= '0' && c <= '9');
 }
