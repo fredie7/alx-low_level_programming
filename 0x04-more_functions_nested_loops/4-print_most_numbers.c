815d04c9105ba455f24250eca6eb9042c9504f4b
fredie7 <lexfrederick3@gmail.com>
2022-06-09 09:50:42 -0700

    File test check

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+i = 0;
+while (i <= 9)
+{
+if (i != 2 && i != 4)
+{
+_putchar(i);
+}
+i++;
+}
+return (0);
+}
