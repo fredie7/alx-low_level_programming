4ef745e084aa904f87b3f12f7865baf5dd837fd5
fredie7 <lexfrederick3@gmail.com>
2022-06-13 08:26:33 -0700

    Write a function that reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 /**
- *void rev_string - a function that reverses a string.
+ *rev_string - a function that reverses a string.
  *@s: value to be evaluate.
  *Return: not.
  */
@@ -13,10 +13,10 @@ int i;
 int word_length;
 int result;
 word_length = strlen(s);
-for (i = 0; i < word_length/2; i++)
+for (i = 0; i < word_length / 2; i++)
 {
-result = s[i]; 
-s[i] = s[word_length - i - 1];  
+result = s[i];
+s[i] = s[word_length - i - 1];
 s[word_length - i - 1] = result;
 }
 }
