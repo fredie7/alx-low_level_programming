069e35e8d1319de0d6ee36e5a9784f4116e77fd1
fredie7 <lexfrederick3@gmail.com>
2022-06-13 12:20:02 -0700

    Generate passwords

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi -  a program that generates random valid passwords.
+ * @s: input string.
+ * Return: integer.
+ */
+int _atoi(char *s)
+{
+unsigned int num = 0, temp = 0, a = 0, b = 1, m = 1, i;
+while (*(s + num) != '\0')
+{
+if (temp > 0 && (*(s + num) < '0' || *(s + num) > '9'))
+break;
+if (*(s + num) == '-')
+b *= -1;
+if ((*(s + num) >= '0') && (*(s + num) <= '9'))
+{
+if (temp > 0)
+m *= 10;
+temp++;
+}
+num++;
+}
+for (i = num - temp; i < num; i += 1)
+{
+a = a + ((*(s + i) - 48) * m);
+m /= 10;
+}
+return (a * b);
+}
