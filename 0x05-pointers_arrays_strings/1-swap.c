b4dd0e427455841dc40057d8730d070ba0c68577
fredie7 <lexfrederick3@gmail.com>
2022-06-13 04:44:51 -0700

    Swap arguments

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,7 +2,8 @@
  *swap_int - swaps the values of two integers.
  * @a: value to be evaluate.
  * @b: value to be evaluate.
- * Return: not.*/
+ * Return: not.
+ */
 void swap_int(int *a, int *b)
 {
 int swap;
