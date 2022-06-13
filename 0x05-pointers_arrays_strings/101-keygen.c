ad7f78028b9995126ede219856908379bf5b7f1d
fredie7 <lexfrederick3@gmail.com>
2022-06-13 13:07:03 -0700

    Generate random valid passwords

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,14 +10,14 @@ int main(void)
 int result = 0, count = 0;
 time_t t;
 srand((unsigned int) time(&t));
-while (count < 2771)
+while (count < 2772)
 {
 result = rand() % 128;
-if ((count + result) > 2771)
+if ((count + result) > 2772)
 break;
 count = count + result;
 printf("%c", result);
 }
-printf("%c\n", (2771 - count));
+printf("%c\n", (2772 - count));
 return (0);
 }
