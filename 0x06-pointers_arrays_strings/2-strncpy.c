f71c02899105213fc50f38e9f4cdf435e15be23a
fredie7 <lexfrederick3@gmail.com>
2022-06-15 03:03:22 -0700

    Copy a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ *char *_strncpy -  a function that copies a string.
+ *@dest: first value to be evaluate.
+ *@src: second value to be evaluate.
+ *@n: third value to be evaluate.
+ *Return: not.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+return (strncpy(dest, src, n));
+}
+
