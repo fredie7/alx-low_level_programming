90ec9e02e65c640c087bccf7b3b5c2ee4afbf485
fredie7 <lexfrederick3@gmail.com>
2022-06-20 04:04:30 -0700

    Copy memory area

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,15 @@
 #include "main.h"
 #include <string.h>
 /**
- * _memset(char *s, char b, unsigned int n) - function that fills memory with a constant byte.
+ * _memset - function
+ * that fills memory with a constant byte.
  * @s: input value
- * @b input value
+ * @b: input value
  * @n: input value
  *
  * Return: a pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-return (memset(s+n,b,b*sizeof(char)));
+return (memset(s + n, b, b * sizeof(char)));
 }
