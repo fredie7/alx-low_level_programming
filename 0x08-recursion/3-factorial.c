ffc168bcb6298938159c42137a16ce5c9bf10831
fredie7 <lexfrederick3@gmail.com>
2022-06-22 05:09:05 -0700

    Print factorial of any number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,20 +2,20 @@
 /**
  * factorial -  returns the factorial of a given number.
  * @n: number to evaluate
- * Return; -1
+ * Return: factorial
  */
 int factorial(int n)
 {
-if (n <= 1)
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 0 || n == 1)
 {
 return (1);
 }
-else if (n >= 1)
-{
-return (n * (n - 1));
-}
 else
 {
-return (-1);
+return (n * factorial(n - 1));
 }
 }
