a3ec5bd6c6d5afde4b873182085f9af4c24b530d
fredie7 <lexfrederick3@gmail.com>
2022-06-24 03:06:22 -0700

    Print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/** main - Prints the name of the program
+ * @argc: defines the length of arguments
+ * @argv: an array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+if (argc == 1)
+{
+printf("%s\n",*argv);
+}
+return (0);
+}
