cd98ec58a8c6b4f6994a21c6b6454b4ab4151582
fredie7 <lexfrederick3@gmail.com>
2022-06-24 10:07:57 -0700

    create a static lib contanining the listed functions

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - concat 2 string
+ * @dest:char
+ * @src:char
+ * Return: 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+char *a = dest;
+while (*dest != '\0')
+{
+dest++;
+}
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return (a);
+}
