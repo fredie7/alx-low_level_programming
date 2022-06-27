b511bdf0a51c7fc09bb525b4380c176ef03723f4
fredie7 <lexfrederick3@gmail.com>
2022-06-27 05:00:50 -0700

    Concatenate all arguments

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * argstostr - Concatenates all the arguments
+ * @ac: allocated memory space
+ * @av: 2D array
+ * Return: array
+ */
+char *argstostr(int ac, char **av)
+{
+char *str;
+int i = 0, j, a, b;
+a = 0;
+b = 0;
+if (ac == 0 || av == NULL)
+{
+return (NULL);
+}
+while (i < ac)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+{
+b++;
+}
+i++;
+}
+str = malloc(b *sizeof(char) + ac + 1);
+if (str == NULL)
+{
+return (NULL);
+}
+while (i < ac)
+{
+for (j = 0; av[i][j] != '\0'; j++, a++)
+{
+str[a] = av[i][j];
+}
+str[a] = '\n';
+a++;
+i++;
+}
+str[a] = '\0';
+return (str);
+}
