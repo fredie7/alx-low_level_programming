218b5a73269ab9b5b263ae07cf8a77ca4e1459f8
fredie7 <lexfrederick3@gmail.com>
2022-07-18 12:36:39 -0700

    Print all emements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,11 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t node = 0;
-while (h != NULL)
+size_t node;
+for (node = 0; h != NULL; node++)
 {
 printf("%d\n", h->n);
 h = h->next;
-node++;
 }
 return (node);
 }
